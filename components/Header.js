b85812783ea6608c5c8aea0b37eb2d644cd94434
collectivewin <hello@collectivewin.ca>
2024-05-17 19:17:41 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 // Header.js
 
 import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between py-4 px-8">
       <div className="flex gap-1 items-center">
         <div className="">
-          <img src="/vinyl-record.png" alt="Icon" width={40} height={40} /> {/* Replace "vinyl-record.png" with your actual icon image */}
+          <Image src="/vinyl-record.png" alt="Icon" width={40} height={40} /> {/* Replace "vinyl-record.png" with your actual icon image */}
         </div>
         <div className="font-medium text-2xl">MINYFY</div>
       </div>
