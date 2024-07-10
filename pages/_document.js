1ca02d309735d29d8c7f6888e17aa715c1d70694
collectivewin <hello@collectivewin.ca>
2024-07-10 19:46:26 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-          <script src="https://www.youtube.com/iframe_api" />
-        </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <Script
+            src="https://www.youtube.com/iframe_api"
+            strategy="afterInteractive"
+          />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
