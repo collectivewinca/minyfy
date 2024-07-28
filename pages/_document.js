2527b238d9d3370a760cd00431e16fccf182f4f8
collectivewin <hello@collectivewin.ca>
2024-07-28 14:12:14 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,25 +7,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-            <link rel="icon" href="/vinyl.png" type="image/png" /> 
-            <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=G-CQSPN22TTV"
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-CQSPN22TTV');
-                `,
-              }}
-            />
+          <link rel="icon" href="/vinyl.png" type="image/png" /> 
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-CQSPN22TTV"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CQSPN22TTV');
+            `}
+          </Script>
           <Script
             src="https://www.youtube.com/iframe_api"
             strategy="afterInteractive"
