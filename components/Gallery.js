053a349fb9c156e856d9d41ee7d98c9aa8a11ee5
collectivewinca <159792879+collectivewinca@users.noreply.github.com>
2024-09-05 06:46:10 -0400

    Update Gallery.js
    Artists updated.

--- components/Gallery.js
+++ components/Gallery.js
@@ -3,28 +3,28 @@ import Image from 'next/image';
 
 const images = [
   {
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/minyfy-e8c97.appspot.com/o/images%2Fminy-1721204117583.jpg?alt=media&token=8f27dc59-7778-4330-b560-d2b518110cc6',
-    link: 'https://minyfy.subwaymusician.xyz/play/JLoRn9KQkc73B7ELFZCB'
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/subway-musician-564bd.appspot.com/o/aminy-generation%2Fminy-0a2b761b-2a29-40fc-a44b-7ae3dee54b03.png?alt=media&token=b017d05f-c168-4998-bc34-a7f54fb498a9',
+    link: 'https://minyfy.subwaymusician.xyz/play/Qe6nRzVhSPWd0IhSsP4g'
   },
   {
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/minyfy-e8c97.appspot.com/o/images%2Fminy-1721203632235.jpg?alt=media&token=d0674793-0f62-4d13-9f21-411d824030a0',
-    link: 'https://minyfy.subwaymusician.xyz/play/6iUnjN7Q4Y4WPOIz0ZvT'
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/subway-musician-564bd.appspot.com/o/aminy-generation%2Fminy-686cc899-48b6-42bb-b474-77f05f668731.png?alt=media&token=b94c09b2-0894-430b-b622-a20bf912e5ca',
+    link: 'https://minyfy.subwaymusician.xyz/play/hR4s0Xgbt4JhbZIOI01X'
   },
   {
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/minyfy-e8c97.appspot.com/o/images%2Fminy-1721154880789.jpg?alt=media&token=44046b6d-08b3-478c-8a25-a02cbe13dfae',
-    link: 'https://minyfy.subwaymusician.xyz/play/xIZr3VYNvKJ0A6x9s8l9'
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/subway-musician-564bd.appspot.com/o/aminy-generation%2Fminy-866326cc-5c79-42ab-9411-a500a2c4ff66.png?alt=media&token=ba1ca12e-5512-466f-9880-3426dd92be5f',
+    link: 'https://minyfy.subwaymusician.xyz/play/2RTSHUOUitZKHPe5tHz3'
   },
   {
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/minyfy-e8c97.appspot.com/o/images%2Fminy-1721204273551.jpg?alt=media&token=b2ee7a26-16ca-4516-bcc6-8cce25fb0776',
-    link: 'https://minyfy.subwaymusician.xyz/play/8Xcu8h60b8OFCSACtbIU'
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/subway-musician-564bd.appspot.com/o/aminy-generation%2Fminy-5e21563c-987d-469b-a958-f5b38b876f72.png?alt=media&token=c03a4464-44a2-4d86-9854-a4d8397f4815',
+    link: 'https://minyfy.subwaymusician.xyz/play/n7ff7fFKLVIRl7ITw8n2'
   },
   {
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/minyfy-e8c97.appspot.com/o/images%2Fminy-1721204350031.jpg?alt=media&token=54179edc-4a7b-424f-a01c-f3a7126a12e2',
-    link: 'https://minyfy.subwaymusician.xyz/play/Bjw2LfBjhcH0yGx3X0ib'
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/subway-musician-564bd.appspot.com/o/aminy-generation%2Fminy-edca1ab3-74b6-473b-a6f2-d2b9acc8606a.png?alt=media&token=d07e0600-0d7f-4f48-ba8b-c4259150b3cc',
+    link: 'https://minyfy.subwaymusician.xyz/play/k0KjOatJ2lZG7oxQ83W6'
   },
   {
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/minyfy-e8c97.appspot.com/o/images%2Fminy-1721204414553.jpg?alt=media&token=d69e2797-73bd-4c7e-a7ef-9c51a4bebac9',
-    link: 'https://minyfy.subwaymusician.xyz/play/O6by6qcLUGVHgrWgUn44'
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/subway-musician-564bd.appspot.com/o/aminy-generation%2Fminy-6051bfd9-30c4-4ce4-8c01-ae2f27b211c2.png?alt=media&token=638bf1a0-1274-459c-92e6-e64640856020',
+    link: 'https://minyfy.subwaymusician.xyz/play/Y8gzr3Mid7DI0OqaRV73'
   }
 ];
 
@@ -57,8 +57,8 @@ const ImageGallery = () => {
 
   return (
     <div className="container mx-auto px-4">
-      {renderImageSection("Top This Week", "Most popular artworks of the week", 0, 3)}
-      {renderImageSection("Featured Artists", "Discover our talented featured artists", 3, 6)}
+      {renderImageSection("Top This Week", "Most popular mixtapes of the week", 0, 3)}
+      {renderImageSection("Featured Artists", "Discover mixtapes from featured artists", 3, 6)}
     </div>
   );
 };
