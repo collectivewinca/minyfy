6a4498e12784d8d0edddf08a536f3c6876c2f99f
collectivewin <hello@collectivewin.ca>
2024-09-17 19:26:44 +0530

    Revert "Update _app.js"
    This reverts commit 6751ef4724dcff5083acf93e8a78c409515ef7ac.

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,14 @@
 import "@/styles/globals.css";
 import Head from 'next/head';
-import { DefaultSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
-        <link rel="icon" href="/favicon.ico" />
-        {/* You can uncomment the line below if you have a specific vinyl icon */}
         {/* <link rel="icon" href="/vinyl.png" type="image/png" /> */}
+        <title>Miny Vinyl</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
-      <DefaultSeo
-        title="Miny Vinyl"
-        description="Discover and collect vinyl records(MINY) with Miny Vinyl - your gateway to a world of music!"
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url: 'https://www.minyvinyl.com/',
-          site_name: 'Miny Vinyl',
-        }}
-       
-      />
       <Component {...pageProps} />
     </>
   );
