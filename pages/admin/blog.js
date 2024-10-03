796023ea854f7001accd420269085932e35ab12d
collective win <hello@collectivewin.ca>
2024-10-03 23:19:35 +0530

    Update blog.js

--- pages/admin/blog.js
+++ pages/admin/blog.js
@@ -138,27 +138,16 @@ function Blog() {
                 class="flex items-center justify-between w-full md:w-auto gap-8 mb-3 md:mb-0"
               >
                 <div class="flex items-center gap-2 text-white">
-                  <div class="size-4">
-                    <svg
-                      viewBox="0 0 48 48"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M44 11.2727C44 14.0109 39.8386 16.3957 33.69 17.6364C39.8386 18.877 44 21.2618 44 24C44 26.7382 39.8386 29.123 33.69 30.3636C39.8386 31.6043 44 33.9891 44 36.7273C44 40.7439 35.0457 44 24 44C12.9543 44 4 40.7439 4 36.7273C4 33.9891 8.16144 31.6043 14.31 30.3636C8.16144 29.123 4 26.7382 4 24C4 21.2618 8.16144 18.877 14.31 17.6364C8.16144 16.3957 4 14.0109 4 11.2727C4 7.25611 12.9543 4 24 4C35.0457 4 44 7.25611 44 11.2727Z"
-                        fill="currentColor"
-                      ></path>
-                    </svg>
-                  </div>
-                  <h2
-                    class="text-white mr-5 text-lg font-bold leading-tight tracking-[-0.015em]"
-                  >
+                  
+                  
                     <a
                       class=""
                       href="https://minyvinyl.com/"
-                      >MINY</a
-                    >
-                  </h2>
+                      >
+                      <div class="size-12 mr-3">
+                            <img src="https://minyfy.subwaymusician.xyz/Minylogo.png" alt="Miny Vinyl Logo" className=" h-12" />
+                        </div>
+                      </a>
                 </div>
                 <label class="flex flex-col min-w-40 !h-10 max-w-64 md:hidden">
                   <div class="flex w-full flex-1 items-stretch rounded-xl h-full">
@@ -906,7 +895,7 @@ function Blog() {
                 Upload
               </button>
               <button 
-                onClick={() => setShowUploadModal(false)}
+                onClick={() => {setShowUploadModal(false), setUploadMessage(null), setUploadFileName('')}}
                 className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               >
                 Cancel
