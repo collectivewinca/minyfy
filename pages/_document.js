5c02e545dd9e693e35e32eab1f326d68b00f1765
collectivewin <hello@collectivewin.ca>
2024-11-16 12:32:32 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,27 +7,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <link rel="icon" href="/vinyl.png" type="image/png" /> 
+          <link rel="icon" href="/vinyl.png" type="image/png" /> 
         </Head>
         <body>
           <Main />
           <NextScript />
           <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-CQSPN22TTV"
+            id="clarity-script"
             strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
+          >
             {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-CQSPN22TTV');
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ozc9phvqpz");
             `}
           </Script>
-          <Script
-            src="https://www.youtube.com/iframe_api"
-            strategy="afterInteractive"
-          />
         </body>
         <Footer />
       </Html>
